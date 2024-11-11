b9486a990706aee5bf90091b2a70cbdedc91d38c
nelicepriatniy <clavik.polyakov@yandex.ru>
2024-11-11 14:32:30 +0300

    preloader

--- src/components/header/Theme.tsx
+++ src/components/header/Theme.tsx
@@ -1,45 +1,45 @@
-import { useEffect, useState } from 'react';
-import s from './style.module.scss'
+import { useEffect, useState, useMemo } from 'react';
+import s from './style.module.scss';
 import { useTheme } from '@/app/ThemeContext';
 
-interface props {
-  text: string,
+interface Props {
+  text: string;
 }
 
-function Theme({text} : props) {
-  // useEffect(() => {
-  //   if(theme === 'dark') {
-  //     setThemeClass([s.light].join(' '))
-  //   } else if(theme === 'light') {
-  //     setThemeClass([s.dark].join(' '))
-  //   } else {
-  //   }
-    
-  //   setIsLoaded(true);
-  // }, [theme]);
+function Theme({ text }: Props) {
+  const { theme, toggleTheme } = useTheme();
+  const [styleClick, setStyleClick] = useState('');
+  const [isLoaded, setIsLoaded] = useState(false);
 
+  // Использование useMemo для классов
+  const themeClass = useMemo(() => {
+    if (theme === 'dark') return s.dark;
+    if (theme === 'light') return s.light;
+    return s.theme;  // или дефолтный класс, если тема не задана
+  }, [theme]);
 
-  // const changeTheme = () => {
-  //   setStyleClick([s.clicked].join(' '));
-  //   setTimeout(() => {
-  //     toggleTheme()
-  //   }, 500);
-  //   setTimeout(() => {
-  //     if(theme === 'light') {
-  //       setThemeClass([s.light].join(' '))
-  //     } else if(theme === 'dark') {
-  //       setThemeClass([s.dark].join(' '))
-  //     }
-  //     setStyleClick([s.notClicked].join(' '));
-  //   }, 2500);
-  // }
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
 
-  return ( 
-    <div className={s.lightBlock}>
-      <p className={s.settingsItem}>{text}</p>
+  const changeTheme = () => {
+    setStyleClick(s.clicked);
+    setTimeout(() => {
+      toggleTheme();
+    }, 500);
+
+    // Убираем класс по завершению
+    setTimeout(() => {
+      setStyleClick(s.notClicked);
+    }, 2500);
+  };
+
+  return (
+    <div className={`${s.lightBlock} ${styleClick} ${themeClass}`}>
+      <p className={s.settingsItem} onClick={changeTheme}>{text}</p>
       <div className={s.lightCyrc}></div>
     </div>
-   );
+  );
 }
 
 export default Theme;
